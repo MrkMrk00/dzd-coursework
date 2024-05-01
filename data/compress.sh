07638cbabaaa706ccc2c44bccb616b6577822734
MrkMrk00 <mhau@outlook.cz>
2024-05-01 23:09:19 +0200

    compressed data?

---
+++ data/compress.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+if ! which zstd > /dev/null 2>&1 ; then
+	echo "Nemam zstd :/"
+	echo "nainstaluj: https://github.com/facebook/zstd"
+
+	exit 1
+fi
+
+if ! which parallel > /dev/null 2>&1 ; then
+	echo "Nemam GNU parallel :/"
+	echo "nainstaluj: https://www.gnu.org/software/parallel/"
+
+	exit 1
+fi
+
+compress() {
+	split --bytes=150M daily_weather.parquet dw-part-
+	FILES=$(ls | grep -E '(.csv)|(dw-part-)')
+	
+	[[ -d ./compressed ]] || mkdir ./compressed
+	
+	parallel --lb 'zstd -f -19 {} -o "compressed/{}.zst"' ::: $FILES
+}
+
+decompress() {
+	FILES=$(ls ./compressed)
+
+	parallel --lb 'zstd -d "./compressed/{}" -o "./{.}"' ::: $FILES
+
+	cat dw-part-* > daily_weather.parquet
+}
+
+case "$1" in
+	"compress")
+		compress
+	;;
+
+	"decompress")
+		decompress	
+	;;
+
+	*)
+		echo "Unknown option"
+	;;
+esac
+
