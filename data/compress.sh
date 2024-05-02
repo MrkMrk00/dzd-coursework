d600888de3c63e0cb2fff6208976422194b82385
MrkMrk00 <mhau@outlook.cz>
2024-05-02 11:30:03 +0200

    split after compression -> better dictionary

--- data/compress.sh
+++ data/compress.sh
@@ -16,24 +16,48 @@ fi
 
 CHECKSUM="05f063ffe73827940f97a3705aef4017"
 
+split_compressed_daily_weather() {
+	pushd . && cd compressed
+
+	split --bytes=48M daily_weather.parquet.zst dw-part-
+	rm daily_weather.parquet.zst
+
+	popd
+}
+
+combine_compressed_daily_weather() {
+	pushd . && cd compressed
+
+	cat dw-part-* > daily_weather.parquet.zst
+	rm dw-part-*
+
+	popd
+}
+
+weather_checksum() {
+	NEW_CHECKSUM=$(md5sum --binary daily_weather.parquet | cut -d' ' -f1)
+	[[ $CHECKSUM == $NEW_CHECKSUM ]] && echo "daily_weather checksum OK" || (echo "daily_weather checksum FAIL"; exit 1)
+}
+
 compress() {
-	split --bytes=50M daily_weather.parquet dw-part-
-	FILES=$(ls | grep -E '(.csv)|(dw-part-)')
+	FILES=$(ls | grep -E '(\.csv)|(\.parquet)')
 	
 	[[ -d ./compressed ]] || mkdir ./compressed
 	
-	parallel --lb 'zstd -f -19 {} -o "compressed/{}.zst"' ::: $FILES
+	parallel --lb 'zstd -f {} -o "compressed/{}.zst"' ::: $FILES
+	split_compressed_daily_weather
 }
 
 decompress() {
+	combine_compressed_daily_weather
+
 	FILES=$(ls ./compressed)
 
 	parallel --lb 'zstd -d -f "./compressed/{}" -o "./{.}"' ::: $FILES
 
 	cat dw-part-* > daily_weather.parquet
 
-	NEW_CHECKSUM=$(md5sum --binary daily_weather.parquet | cut -d' ' -f1)
-	[[ $CHECKSUM == $NEW_CHECKSUM ]] && echo "daily_weather checksum OK" || (echo "daily_weather checksum FAIL"; exit 1)
+	weather_checksum
 }
 
 case "$1" in
